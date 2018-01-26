15b07a0bed9f3afcdbe2ef41a914dce3ddd2b48e
paulnguyen <blackmoon51291@gmail.com>
2018-01-26 19:40:52 +0700

    add code for the interfaces

---
+++ app/src/main/java/vn/realtest/stock/justanewproject/Adapter/ViewPagerAdapterMarket.java
@@ -0,0 +1,60 @@
+package vn.realtest.stock.justanewproject.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import vn.realtest.stock.justanewproject.Fragments.FavoriteFragment;
+import vn.realtest.stock.justanewproject.Fragments.HnxFragment;
+import vn.realtest.stock.justanewproject.Fragments.HoseFragment;
+import vn.realtest.stock.justanewproject.Fragments.UpcomFragment;
+
+/**
+ * Created by Admin on 1/21/2018.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter{
+    private static int NUM_ITEMS = 4;
+
+    public ViewPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    // Returns total number of pages
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    // Returns the fragment to display for that page
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FavoriteFragment();
+            case 1:
+                return new HnxFragment();
+            case 2:
+                return new HoseFragment();
+            case 3:
+                return new UpcomFragment();
+        }
+        return null;
+    }
+
+    // Returns the page title for the top indicator
+    @Override
+    public CharSequence getPageTitle(int position) {
+       switch (position){
+           case 0:
+               return "Favorite";
+           case 1:
+               return "HNX";
+           case 2:
+               return "HOSE";
+           case 3:
+               return "UPCOM";
+       }
+        return null;
+    }
+}
---
+++ app/src/main/java/vn/realtest/stock/justanewproject/Fragments/HoseFragment.java
@@ -0,0 +1,8 @@
+package vn.realtest.stock.justanewproject.Fragments;
+
+/**
+ * Created by Admin on 1/21/2018.
+ */
+
+public class HoseFragment {
+}
